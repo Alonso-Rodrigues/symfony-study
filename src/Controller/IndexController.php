951b0e58033fc0031b2969050cb18759c0b35d08
Alonso <alonso.rbass@gmail.com>
2024-01-30 22:23:28 +0100

    update

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -11,8 +11,29 @@ class IndexController extends AbstractController
     #[Route('/', name: 'app_index')]
     public function index(): Response
     {
-        return $this->render('index/index.html.twig', [
+        $posts = $this->getPosts();
+        
+        return $this->render('index/index.html.twig',[
             'controller_name' => 'IndexController',
         ]);
     }
+    
+    #[Route('/post/{slug?}', name: 'app_show')]
+    public function show(string $slug): Response
+    {
+        
+        return $this->render('index/single.html.twig', compact('slug'));
+    }
+
+    private function getPosts(): array
+    {
+        return[
+            ['id' => 1, 'title' => 'Teste Postagem 1'],
+            ['id' => 2, 'title' => 'Teste Postagem 2'],
+            ['id' => 3, 'title' => 'Teste Postagem 3'],
+            ['id' => 4, 'title' => 'Teste Postagem 4'],
+            ['id' => 5, 'title' => 'Teste Postagem 5']
+        ];
+    }
+    
 }
